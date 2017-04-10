74ab29a705c11ddf236f257baf94748c7be1bd72
Kathy Toufighi <kathy.toufighi@gmail.com>
2017-04-10 10:41:58 -0400

    updated new images, deleted old

--- script.js
+++
@@ -1,8 +0,0 @@
-const nav = document.querySelector("#button");
-const topOfbutton = nav.offsetTop;
-
-function fixNav() {
-  console.log(topOfbutton);
-}
-
-window.addEventListener('scroll', fixNav);
